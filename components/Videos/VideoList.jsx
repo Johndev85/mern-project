6dc2d9446d3af8b163528d1a04acc6c92f93047c
John Botero <jhondark@gmail.com>
2020-10-29 23:42:37 -0500

    cardvideo updated, video sort implemented

--- components/Videos/VideoList.jsx
+++ components/Videos/VideoList.jsx
@@ -2,7 +2,7 @@ import styles from "./videosList.module.scss"
 import { useEffect, useState } from "react"
 import * as videoService from "../../services/videoService"
 import Loader from "../../components/Loader"
-import ReactPlayer from "react-player/youtube"
+import CardVideo from "../../components/CardVideo/CardVideo"
 
 const VideoList = () => {
     const [videos, setVideos] = useState([])
@@ -10,7 +10,20 @@ const VideoList = () => {
 
     const loadVideos = async () => {
         const res = await videoService.getVideos()
-        setVideos(res.data)
+        const formatedVideos = res.data
+            .map((video) => {
+                return {
+                    ...video,
+                    createdAt: video.created_at
+                        ? new Date(video.created_at)
+                        : new Date(),
+                    updatedAt: video.updatedAt
+                        ? new Date(video.updatedAt)
+                        : new Date(),
+                }
+            })
+            .sort((a, b) => b.createdAt.getTime() - a.updatedAt.getTime())
+        setVideos(formatedVideos)
         setLoad(true)
     }
 
@@ -24,25 +37,9 @@ const VideoList = () => {
         </div>
     ) : videos.length !== 0 ? (
         <section className={styles.container}>
-            {videos.map((video) => {
-                return (
-                    <div className={styles.container__cardVid}>
-                        <div className={styles.container__cardVid__title}>
-                            <h2>{video.title}</h2>
-                            <img src="/images/delete.svg" alt="delete-icon" />
-                        </div>
-                        <div className={styles.container__cardVid__body}>
-                            <ReactPlayer
-                                url={video.url}
-                                width={300}
-                                height={200}
-                            />
-                            <h3>{video.description}</h3>
-                            <span> {video.created_at}</span>
-                        </div>
-                    </div>
-                )
-            })}
+            {videos.map((video) => (
+                <CardVideo video={video} key={video._id} />
+            ))}
         </section>
     ) : (
         <div className={styles.noVideos}>
--- services/videoService.jsx
+++ services/videoService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = "http://localhost:3001"
+const API = "https://api-mern-project.herokuapp.com"
 
 export const getVideos = async () => {
     try {
